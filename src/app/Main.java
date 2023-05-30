50bb2413caa732b208ac5b02558c3f0811a66c47
Niki4570 <nkrivoruchko125133@gmail.com>
2023-05-30 22:17:01 +0300

    V.2

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,5 +1,14 @@
 package app;
 
 public class Main {
-
+    public static void main(String[] args) {
+        KgToLbs(7);
+        LbsToKg(5);
+    }
+    static void KgToLbs(int kg){
+        System.out.println(kg * 2.205);
+    }
+    static void LbsToKg(int lbs){
+        System.out.println(lbs * 2.205);
+    }
 }
